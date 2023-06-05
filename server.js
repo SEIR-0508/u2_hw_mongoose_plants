1aa703edf16d5b04f302cc5ac324a08e1d59454f
Andrew <mullinsa428@gmail.com>
2023-06-04 22:28:57 -0400

    finished the initial homework. Some things seemed to be missing from the readme like the section on creating a test plant did not show up anywehre for me. There also seemed to be elements of the bonus section that were exprected to be completed before getting into the bonus (things like setting up body-parser doesn't appear until the bonus section, but it is shown in the getPlantById section as something that should already exist in the server.js file. This was causing a bit of confusion for me so I've left it out for now. Also didn't see instruction for setting up a router/index.js file so I've put all my app. commands in the server.js file

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const db = require('./db');
+const plantController = require('./controllers/plantController.js')
+const logger = require('morgan');
+// require() imports and middleware here ^ ///////
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+// app.use() middleware here ^ ///////////////////
+
+app.use(logger('dev'))
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`))
+
+
+
+app.get('/', (req, res) => res.send('This is root!'))
+
+// the readme says to put the below text in routes, but i think it should be going here instead
+
+app.get('/plants/:id', plantController.getPlantById)
+
+app.get('/plants', plantController.getAllPlants)
+app.post('/plants', plantController.createPlant)
